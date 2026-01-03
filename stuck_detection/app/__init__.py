fff11074af5b74a239f9601409da37a234a697cd
yehan2002 <yehanjaya2002@gmail.com>
2026-01-03 13:38:40 +0530

    stuck_detection: initialize project.

--- stuck_detection/app/__init__.py
+++ stuck_detection/app/__init__.py
@@ -1,9 +1,37 @@
+import asyncio
+from contextlib import asynccontextmanager
+
+from shared import rabbitmq
+
+from .dto import ASRData
 from .config import config
+from aio_pika.abc import AbstractChannel
 
 __all__ = ["app", "config"]
 
 from fastapi import FastAPI
 
-app = FastAPI()
 
-print(config)
+async def read_queue(conn: AbstractChannel):
+    try:
+        queue_reader = rabbitmq.read_queue(conn, "ASR_stream", ASRData)
+        async for data in queue_reader:
+            print(data)
+    except Exception as e:
+        print(e)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with rabbitmq.connect(config.rabbitmq_url) as con:
+        task = asyncio.create_task(read_queue(con))
+        yield
+        task.cancel()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/")
+async def main():
+    return "Running"
--- stuck_detection/app/config.py
+++ stuck_detection/app/config.py
@@ -1,14 +1,15 @@
 from pydantic import Field
+
 __all__ = ["Settings", "config"]
 
 from shared import SharedBaseSettings
 
 
 class Settings(SharedBaseSettings):
-    port: int= Field()
+    port: int = Field()
+    rabbitmq_url: str = Field()
 
     # add other config keys here
-
 
 
 config = Settings.load()

d2ff2950081f1040cca61d32f4504a5dc01cfe0b
yehan2002 <yehanjaya2002@gmail.com>
2026-02-24 21:33:07 +0530

    common: cleanup queue code.

--- common/src/mq/mod.rs
+++ common/src/mq/mod.rs
@@ -1,6 +1,5 @@
 mod error;
 mod queue;
-pub mod rpc;
 
 pub use error::{MQError, Result};
 pub use queue::*;
--- common/src/mq/queue.rs
+++ common/src/mq/queue.rs
@@ -4,33 +4,44 @@ use crate::mq::error::Result;
 use async_rs::Runtime;
 use futures_lite::StreamExt;
 use lapin::{
-    BasicProperties, Channel, Connection, ConnectionProperties, Consumer, PublisherConfirm,
-    options::{BasicAckOptions, BasicConsumeOptions, BasicPublishOptions, QueueDeclareOptions},
+    BasicProperties, Channel, ConnectionProperties, Consumer, PublisherConfirm,
+    options::{
+        BasicAckOptions, BasicConsumeOptions, BasicPublishOptions, QueueBindOptions,
+        QueueDeclareOptions,
+    },
     types::FieldTable,
 };
 use serde::{Deserialize, Serialize, de::DeserializeOwned};
 use tokio::sync;
 
+#[derive(Debug, Deserialize, Clone)]
+pub struct Config {
+    pub connection_name: String,
+    pub address: String,
+}
+
 #[derive(Debug, Clone)]
-pub struct QueueConnection {
-    channel: Channel,
-    queue_name: String,
+pub struct Connection {
+    pub(crate) channel: Channel,
 }
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct QueueConfig {
-    connection_name: String,
-    address: String,
-    queue_name: String,
+    pub queue_name: Option<String>,
+    pub exchange_name: Option<String>,
+    pub create_queue: bool,
 }
 
+#[derive(Debug)]
 /// Queue is a rabbitmq queue that that has only one type of message
 pub struct Queue<T>
 where
     T: Serialize + DeserializeOwned + Sized,
 {
-    con: QueueConnection,
+    con: Connection,
     consumer: sync::OnceCell<Consumer>,
+    queue_name: String,
+    exchange_name: String,
 
     _msg_type: std::marker::PhantomData<T>,
 }
@@ -39,9 +50,49 @@ impl<T> Queue<T>
 where
     T: Serialize + DeserializeOwned + Sized,
 {
-    pub async fn create(cfg: QueueConfig) -> Result<Queue<T>> {
-        let con = connect(cfg, true).await?;
-        Self::from_connection(con)
+    pub async fn create(con: Connection, cfg: QueueConfig) -> Result<Queue<T>> {
+        let mut queue_name = cfg.queue_name.unwrap_or("".to_owned());
+
+        if cfg.create_queue {
+            let mut queue_config = QueueDeclareOptions::default();
+            queue_config.auto_delete = true;
+
+            let queue = con
+                .channel
+                .queue_declare(
+                    queue_name.as_str().into(),
+                    queue_config,
+                    FieldTable::default(),
+                )
+                .await?;
+
+            if queue_name.is_empty() {
+                queue_name = queue.name().to_string();
+            }
+
+            log::debug!("Created queue {}: {queue:?}", queue_name);
+
+            if let Some(ref exchange) = cfg.exchange_name {
+                con.channel
+                    .queue_bind(
+                        queue_name.as_str().into(),
+                        exchange.as_str().into(),
+                        "".into(),
+                        QueueBindOptions::default(),
+                        FieldTable::default(),
+                    )
+                    .await?;
+                log::debug!("Created queue {}: {queue:?}", queue_name);
+            }
+        }
+
+        Ok(Self {
+            con,
+            consumer: sync::OnceCell::new(),
+            queue_name: queue_name,
+            exchange_name: cfg.exchange_name.unwrap_or("".to_owned()),
+            _msg_type: PhantomData,
+        })
     }
 
     /// sends a message on the channel.
@@ -65,8 +116,8 @@ where
             .con
             .channel
             .basic_publish(
-                "".into(),
-                self.con.queue_name.as_str().into(),
+                self.exchange_name.as_str().into(),
+                self.queue_name.as_str().into(),
                 BasicPublishOptions::default(),
                 &payload,
                 BasicProperties::default(),
@@ -87,10 +138,11 @@ where
 
     pub async fn read_inner(&mut self) -> Result<Option<T>> {
         self.consumer
-            .get_or_try_init(async || Self::create_consumer(&self.con, "consumer").await)
+            .get_or_try_init(async || {
+                Self::create_consumer(&self.con, &self.queue_name, "consumer").await
+            })
             .await?;
         let consumer = self.consumer.get_mut().expect("should be init");
-
         let delivery = consumer.next().await;
         if let Some(maybe_message) = delivery {
             let message = maybe_message?;
@@ -104,90 +156,76 @@ where
     }
 
     async fn create_consumer(
-        con: &QueueConnection,
+        con: &Connection,
+        queue_name: &str,
         name: &str,
     ) -> std::result::Result<Consumer, lapin::Error> {
         con.channel
             .basic_consume(
-                con.queue_name.as_str().into(),
-                format!("{}_consumer_{name}", con.queue_name).into(),
+                queue_name.into(),
+                format!("{}_consumer_{name}", queue_name).into(),
                 BasicConsumeOptions::default(),
                 FieldTable::default(),
             )
             .await
     }
-
-    fn from_connection(con: QueueConnection) -> Result<Queue<T>> {
-        Ok(Self {
-            con,
-            consumer: sync::OnceCell::new(),
-            _msg_type: PhantomData,
-        })
-    }
 }
 
-async fn connect(cfg: QueueConfig, create_queue: bool) -> Result<QueueConnection> {
-    let runtime = Runtime::tokio_current();
+impl Connection {
+    pub async fn for_config(cfg: Config) -> Result<Connection> {
+        let runtime = Runtime::tokio_current();
 
-    let config = ConnectionProperties::default()
-        .with_connection_name(cfg.connection_name.into())
-        .enable_auto_recover()
-        .configure_backoff(|backoff| {
-            backoff.with_max_times(3);
-        });
+        let config = ConnectionProperties::default()
+            .with_connection_name(cfg.connection_name.into())
+            .enable_auto_recover()
+            .configure_backoff(|backoff| {
+                backoff.with_max_times(3);
+            });
 
-    let conn = Connection::connect_with_runtime(&cfg.address, config, runtime.clone()).await?;
+        let connection =
+            lapin::Connection::connect_with_runtime(&cfg.address, config, runtime.clone()).await?;
 
-    let channel = conn.create_channel().await?;
+        let channel = connection.create_channel().await?;
 
-    if create_queue {
-        let mut queue_config = QueueDeclareOptions::default();
-        queue_config.auto_delete = true;
-
-        let queue = channel
-            .queue_declare(
-                cfg.queue_name.clone().into(),
-                queue_config,
-                FieldTable::default(),
-            )
-            .await?;
-
-        log::debug!("Created queue {}: {queue:?}", cfg.queue_name);
+        Ok(Connection { channel })
     }
-
-    Ok(QueueConnection {
-        channel,
-        queue_name: cfg.queue_name,
-    })
 }
 
 #[cfg(test)]
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
-struct TestMessage {
+pub(crate) struct TestMessage {
     content: String,
 }
+
 #[cfg(test)]
-async fn create_test_connection() -> QueueConnection {
-    let cfg = QueueConfig {
+pub(crate) async fn create_test_connection() -> Connection {
+    let cfg = Config {
         connection_name: "test".to_string(),
         address: "amqp://appuser:apppass@127.0.0.1".to_string(),
-        queue_name: "test-queue".to_string(),
     };
 
-    connect(cfg, true).await.unwrap()
+    Connection::for_config(cfg).await.unwrap()
 }
 
 #[tokio::test]
 async fn test_queue() {
+    let queue_config = QueueConfig {
+        queue_name: Some("test_queue".to_owned()),
+        exchange_name: None,
+        create_queue: true,
+    };
+
     let message = TestMessage {
         content: "Hello".to_owned(),
     };
     let con = create_test_connection().await;
 
-    let sender = Queue::from_connection(con.clone()).unwrap();
+    let sender = Queue::create(con.clone(), queue_config.clone())
+        .await
+        .unwrap();
     sender.send(&message).await.unwrap();
 
-    let mut consumer = Queue::from_connection(con.clone()).unwrap();
+    let mut consumer = Queue::create(con.clone(), queue_config).await.unwrap();
     let data: TestMessage = consumer.read().await.unwrap().unwrap();
     assert!(message == data);
 }
@@ -197,6 +235,12 @@ async fn test_queue_sync() {
     use std::sync::Arc;
     use tokio::sync::Mutex;
 
+    let queue_config = QueueConfig {
+        queue_name: Some("test_queue".to_owned()),
+        exchange_name: None,
+        create_queue: true,
+    };
+
     let con = create_test_connection().await;
 
     let message = TestMessage {
@@ -205,7 +249,9 @@ async fn test_queue_sync() {
 
     let cond = Arc::new(Mutex::new(false));
 
-    let sender = Queue::from_connection(con.clone()).unwrap();
+    let sender = Queue::create(con.clone(), queue_config.clone())
+        .await
+        .unwrap();
     let message_copy = message.clone();
 
     let cond_reader = cond.clone();
@@ -217,7 +263,7 @@ async fn test_queue_sync() {
         assert!(value == true, "send returned before read")
     });
 
-    let mut consumer = Queue::from_connection(con.clone()).unwrap();
+    let mut consumer = Queue::create(con.clone(), queue_config).await.unwrap();
 
     *cond_writer = true;
     drop(cond_writer);

aad6225b7caad59e705bbc06402238f80a75c670
yehan2002 <yehanjaya2002@gmail.com>
2026-02-26 13:27:25 +0530

    common: add mq dtos.

--- common/src/dto/mod.rs
+++ common/src/dto/mod.rs
@@ -1,1 +1,20 @@
+use serde::{Deserialize, Serialize};
+
 pub mod asr;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GameFeatures {
+    pub stress: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SessionCreate {
+    pub session_id: uuid::Uuid,
+    pub features: GameFeatures,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type", content = "data", rename_all = "snake_case")]
+pub enum MQMessage {
+    ASR(asr::ASR),
+}

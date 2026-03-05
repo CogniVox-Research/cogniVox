df8c392c31615102c0158f234058761d2c61ac5e
navindunimsara2001 <navidunimsara@gmail.com>
2026-03-05 20:09:15 +0530

    ASR type

--- web_ui/src/lib/types.ts
+++ web_ui/src/lib/types.ts
@@ -1,3 +1,4 @@
+/** ================= Settings =============== */
 export type AudienceDifficulty = "easy" | "medium" | "hard";
 
 export type SceneType =
@@ -24,3 +25,48 @@ export type GameSettings = SceneType & {
     distractions: boolean;
     difficulty: AudienceDifficulty;
 };
+
+/** ================= ASR =============== */
+export type Token = {
+    text: string;
+    probability: number;
+};
+
+export type Timestamp = {
+    start: number;
+    end: number;
+};
+
+export type Segment = {
+    text: string;
+    tokens: Token[];
+    probability: number;
+    timestamp: Timestamp;
+};
+
+export type Silence = {
+    timestamp: Timestamp;
+};
+
+export type ResultType = "partial" | "complete";
+
+export type Line =
+    | ({
+        type: "complete";
+    } & Segment)
+    | ({
+        type: "partial";
+    } & Segment)
+    | {
+        type: "silence";
+        timestamp: Timestamp;
+    };
+
+export type ASR = {
+    type: ResultType;
+    session_id: string;
+    lines: Line[];
+    full_text: string;
+    current_silence: Silence | null;
+};
+

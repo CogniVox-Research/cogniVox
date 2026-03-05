8458025ef948117832dc1ec0e8e31ac38718fa79
navindunimsara2001 <navidunimsara@gmail.com>
2026-03-05 20:09:48 +0530

    stress req type

--- web_ui/src/lib/types.ts
+++ web_ui/src/lib/types.ts
@@ -70,3 +70,30 @@ export type ASR = {
     current_silence: Silence | null;
 };
 
+/** ================= Stress Request ================ */
+
+export type StressRequest = {
+    eda_mean?: number | null;
+    eda_std?: number | null;
+    eda_min?: number | null;
+    eda_max?: number | null;
+    bvp_mean: number;
+    bvp_std: number;
+    temp_mean?: number | null;
+    temp_std?: number | null;
+    acc_mag_mean?: number | null;
+    acc_mag_std?: number | null;
+
+    // Lite specific
+    bvp_min?: number | null;
+    bvp_max?: number | null;
+    bvp_range?: number | null;
+    bvp_energy?: number | null;
+    acc_mean?: number | null;
+    acc_std?: number | null;
+    acc_max?: number | null;
+
+    session_id?: string | null;
+};
+
+

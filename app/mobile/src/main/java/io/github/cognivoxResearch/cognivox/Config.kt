32c239fc8a8a02a55baf0db5f96f18377d978619
yehan2002 <yehanjaya2002@gmail.com>
2026-03-07 19:35:14 +0530

    app: update urls and fix dto issue.

--- app/mobile/src/main/java/io/github/cognivoxResearch/cognivox/Config.kt
+++ app/mobile/src/main/java/io/github/cognivoxResearch/cognivox/Config.kt
@@ -1,8 +1,8 @@
 package io.github.cognivoxResearch.cognivox
 
 const val API_HOST = "192.168.1.100:7000"
-const val DEVICE_URL = "ws/device"
-const val SESSION_URL = "ws/game"
+const val DEVICE_URL = "api/ws/device"
+const val SESSION_URL = "api/ws/game"
 const val RETRY_DELAY = 5000L
 
 const val PREF_TAG = "CG"

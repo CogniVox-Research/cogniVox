a99c44bbc60e817379fea8e4d77b4261cd2d267a
yehan2002 <yehanjaya2002@gmail.com>
2026-03-14 02:55:00 +0530

    docker: cleanup dockerfiles & add script for auto publishing images.

--- services/asr/scripts/publish-asr-cuda.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-docker build -t ghcr.io/cognivox-research/asr_service_cuda_13_0:latest -f ./cuda.Dockerfile ../../
-docker push ghcr.io/cognivox-research/asr_service_cuda_13_0:latest
--- services/asr/scripts/publish-asr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-SCRIPT_DIR=`realpath "$(dirname "$0")"`
-IMAGE_DIR=`dirname "$SCRIPT_DIR"`
-cd $IMAGE_DIR
-
-docker build -t ghcr.io/cognivox-research/asr_service:latest -f ./Dockerfile ../../
-docker push ghcr.io/cognivox-research/asr_service:latest
